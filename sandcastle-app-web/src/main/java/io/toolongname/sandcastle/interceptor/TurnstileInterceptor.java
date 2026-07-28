7c436c7b00d7d4ea3110ab125a025e23298fd44b
Zixu.Deng <admin@dengzixu.com>
2026-07-29 00:08:42 +0800

    💥 修改数据返回

--- sandcastle-app-web/src/main/java/io/toolongname/sandcastle/interceptor/TurnstileInterceptor.java
+++ sandcastle-app-web/src/main/java/io/toolongname/sandcastle/interceptor/TurnstileInterceptor.java
@@ -3,6 +3,7 @@ package io.toolongname.sandcastle.interceptor;
 import io.toolongname.sandcastle.component.SecurityComponent;
 import io.toolongname.sandcastle.model.ResponseData;
 import io.toolongname.sandcastlecommon.misc.enums.ErrorCode;
+import io.toolongname.sandcastlecommon.misc.exception.general.HumanMachineVerificationFailedException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
@@ -22,27 +23,13 @@ public class TurnstileInterceptor implements HandlerInterceptor {
         String header = request.getHeader("x-cloudflare-turnstile");
 
         if (null == header || header.isBlank()) {
-            ResponseData responseData = ResponseData
-                    .FAILED(ErrorCode.HUMAN_MACHINE_VERIFICATION_FAILED.message(), ErrorCode.HUMAN_MACHINE_VERIFICATION_FAILED.code());
-
-            response.setHeader("Content-Type", "application/json;charset=utf-8");
-            response.getWriter().write(new ObjectMapper().writeValueAsString(responseData));
-            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
-
-            return false;
+            throw new HumanMachineVerificationFailedException();
         }
 
         if (securityComponent.turnstileVerify(header)) {
             return true;
         } else {
-            ResponseData responseData = ResponseData
-                    .FAILED(ErrorCode.HUMAN_MACHINE_VERIFICATION_FAILED.message(), ErrorCode.HUMAN_MACHINE_VERIFICATION_FAILED.code());
-
-            response.setHeader("Content-Type", "application/json;charset=utf-8");
-            response.getWriter().write(new ObjectMapper().writeValueAsString(responseData));
-            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
-
-            return false;
+            throw new HumanMachineVerificationFailedException();
         }
     }
 }
--- sandcastle-app-web/src/main/java/io/toolongname/sandcastle/web/ExceptionHandlerControllerAdvice.java
+++ sandcastle-app-web/src/main/java/io/toolongname/sandcastle/web/ExceptionHandlerControllerAdvice.java
@@ -1,56 +1,56 @@
 package io.toolongname.sandcastle.web;
 
-import io.toolongname.sandcastle.model.ResponseData;
 import io.toolongname.sandcastlecommon.misc.enums.ErrorCode;
 import io.toolongname.sandcastlecommon.misc.exception.BusinessException;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingRequestHeaderException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class ExceptionHandlerControllerAdvice {
     @ExceptionHandler(exception = ServletException.class)
-    public ResponseEntity<ResponseData> a(ServletException servletException, HttpServletRequest req, HttpServletResponse resp) {
+    public ProblemDetail ServletExceptionHandler(ServletException servletException, HttpServletRequest req, HttpServletResponse resp) {
         return switch (servletException) {
-            case NoResourceFoundException _ ->
-                    ResponseEntity.status(HttpStatus.NOT_FOUND).body(ResponseData.FAILED("路由 [" + req.getServletPath() + "] 不存在"));
+            case NoResourceFoundException _ -> ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, "路由不存在");
             case MissingRequestHeaderException e ->
-                    ResponseEntity.badRequest().body(ResponseData.FAILED("缺少请求头 [" + e.getHeaderName() + "]"));
+                    ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, "缺少请求头 [" + e.getHeaderName() + "]");
             default ->
-                    ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ResponseData.FAILED("未知错误, " + servletException.getMessage()));
+                    ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR, servletException.getMessage());
         };
     }
 
     @ExceptionHandler(exception = MethodArgumentNotValidException.class)
-    public ResponseEntity<ResponseData> methodArgumentNotValidExceptionHandler(MethodArgumentNotValidException e) {
-        Map<String, String> errorMsg = new HashMap<>();
+    public ProblemDetail methodArgumentNotValidExceptionHandler(MethodArgumentNotValidException e) {
+        Map<String, String> errorFields = new HashMap<>();
         e.getFieldErrors().forEach(fieldError -> {
-            errorMsg.put(fieldError.getField(), fieldError.getDefaultMessage());
+            errorFields.put(fieldError.getField(), fieldError.getDefaultMessage());
         });
 
-        return ResponseEntity
-                .badRequest()
-                .body(new ResponseData(ResponseData.Status.FAILED,
-                        ErrorCode.DATA_VALIDATION_FAILED.code(),
-                        ErrorCode.DATA_VALIDATION_FAILED.message(),
-                        errorMsg));
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, ErrorCode.DATA_VALIDATION_FAILED.message());
+        problemDetail.setProperty("error_code", ErrorCode.DATA_VALIDATION_FAILED.code());
+        problemDetail.setProperty("error_fields", errorFields);
+
+        return problemDetail;
     }
 
     @ExceptionHandler(exception = BusinessException.class)
-    public ResponseEntity<ResponseData> businessExceptionHandler(BusinessException e) {
+    public ProblemDetail businessExceptionHandler(BusinessException e) {
         ErrorCode code = e.getErrorCode();
 
-        return ResponseEntity.status(e.getHttpStatus())
-                .body(ResponseData.FAILED(code.message(), code.code()));
+        ProblemDetail problemDetail = ProblemDetail.forStatus(e.getHttpStatus());
+        problemDetail.setDetail(e.getMessage());
+        problemDetail.setProperty("error_code", code.code());
+
+        return problemDetail;
     }
 }
